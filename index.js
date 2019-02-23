c116de5937a72cebe2f89c65504ff6581b4720a3
Curtis Lowder <cwlowder@users.noreply.github.com>
2019-02-23 12:20:26 -0600

    Calling now works (#7)
    * Upload file and copy to clipboard
    
    * Added animation
    
    * Merging code

--- index.js
+++ index.js
@@ -1,12 +1,81 @@
-exports.decorateTerms = (Terms, {React}) => {
-return class extends React.Component {
+const fs = require('fs');
+const path = require('path')
+const {spawn} = require('child_process')
+const {shell} = require('electron')
+var ncp = require("copy-paste")
+
+exports.decorateTerms = (Terms, {React, notify}) => {
+  return class extends React.Component {
+
 	constructor(props, context) {
 		super(props, context);
 		this.terms = null;
 		this.onDecorated = this.onDecorated.bind(this);
 		this.state = {
-			isRecording: false
+			isRecording: false,
 		}
+	}
+
+	componentDidUpdate(_,prevState) {
+		if (!this.state.isRecording && prevState.isRecording){
+			// Make a temp working DIRECTORY!!!!!!!!!!
+			const dir = path.resolve(__dirname, './.tmp');
+			const fileName = "text.txt"
+
+			if (!fs.existsSync(dir)){
+				fs.mkdirSync(dir);
+			}
+
+			// Write the target file to disk
+			fs.writeFileSync( dir + '/' + fileName, 'hello i am not a secret chicken 🐔', 'utf8')
+
+			// Write a `now.json` in this directory
+			const nowConfig = {
+				version: 2,
+				builds: [
+					{ src: fileName, use: "@now/static" }
+				]
+			}
+
+			fs.writeFileSync( dir + '/now.json', JSON.stringify(nowConfig, null, 2), 'utf8')	
+			console.log(__dirname)
+			var child = spawn(path.resolve(__dirname,'./node_modules/now/download/dist/now'), [ path.resolve(__dirname,'./.tmp/')]);
+
+			child.stdout.on('data', (data) => {
+				console.log(`stdout: ${data}`)
+				this._notifyVideoUploaded(data)
+			})
+
+			child.stderr.on('data', (data) => {
+				console.log(`stderr: ${data}`);
+			});
+
+			child.on('close', (code) => {
+				console.log(`child process exited with code ${code}`);
+			});
+
+			
+			
+		}
+	}
+
+	_notifyVideoUploaded(nowVideo) {
+		console.log("RUNNING!!!!!!!!!!!!!!!!!!!!" + nowVideo)
+		this.setState({ deployedUrl: nowVideo })
+
+		ncp.copy(nowVideo)
+
+		let videoNotification = new Notification('Your "video" is online at', {
+			body: nowVideo
+		})
+		
+		videoNotification.onclick = () => {
+			shell.openExternal(nowVideo)
+		}
+	}
+
+	componentWillUnmount(){
+		clearTimeout(this.timeOutId)
 	}
 
 	onDecorated(terms) {
@@ -49,7 +118,8 @@ return class extends React.Component {
 					boxShadow: '0 0 5px red',
 					backgroundColor: 'red',
 				}
-			}), React.createElement('style', {key: 3}, `@keyframes blink-motion { 0% { opacity: .1; } 50% { opacity: 1; } 100% { opacity: 0.1; } }`)
+			}),
+			React.createElement('style', {key: 3}, `@keyframes blink-motion { 0% { opacity: .1; } 50% { opacity: 1; } 100% { opacity: 0.1; } }`)
 		];
 	}
   }
