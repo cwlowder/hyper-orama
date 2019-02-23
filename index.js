22588b677b6b19209c5819dcb22a9c72e8137634
Kevin Mui <kmui2@wisc.edu>
2019-02-23 12:41:57 -0600

    added file recording

--- index.js
+++ index.js
@@ -1,64 +1,82 @@
-exports.decorateTerms = (Terms, {React}) => {
-return class extends React.Component {
-	constructor(props, context) {
-		super(props, context);
-		this.terms = null;
-		this.onDecorated = this.onDecorated.bind(this);
-		this.state = {
-			isRecording: false
-		}
-	}
+const recording = require('./recording');
 
-	onDecorated(terms) {
-		this.terms = terms;
+exports.decorateTerms = (Terms, { React }) => {
+  return class extends React.Component {
+    constructor(props, context) {
+      super(props, context);
+      this.terms = null;
+      this.onDecorated = this.onDecorated.bind(this);
+      this.state = {
+        isRecording: false
+      };
+    }
 
-		this.terms.registerCommands({
-			"window:togglerecord": e => {
-				// e parameter is React key event
-				e.preventDefault();
-				console.log("PRESSED")
-				this.setState((prevState) => ({ isRecording: !prevState.isRecording }))
-			}
-		});
+    onDecorated(terms) {
+      this.terms = terms;
 
-		// Don't forget to propagate it to HOC chain
-		if (this.props.onDecorated) this.props.onDecorated(terms);
-	}
+      this.terms.registerCommands({
+        'window:togglerecord': e => {
+          // e parameter is React key event
+          e.preventDefault();
+          console.log('PRESSED');
+          if (!this.state.isRecording) {
+            recording.startRecording();
+          } else {
+            recording.stopRecording();
+          }
+          this.setState(prevState => ({ isRecording: !prevState.isRecording }));
+        }
+      });
 
-	render() {
-		return [
-			React.createElement(
-				Terms,
-				Object.assign({}, this.props, {
-					onDecorated: this.onDecorated,
-					key: 1,
-				})
-			),
-			this.state.isRecording && React.createElement("div", {
-				key: 2,
-				className: "IsRecording",
-				style: {
-					animation: 'blink-motion 1s infinite',
-					position: 'absolute',
-					borderRadius: '50%',
-					top: document.querySelector('.header_appTitle').getBoundingClientRect().top + 2,
-					left: document.querySelector('.header_appTitle').offsetLeft - 16,
-					width: 9,
-					height: 9,
-					border: '1px solid black',
-					boxShadow: '0 0 5px red',
-					backgroundColor: 'red',
-				}
-			}), React.createElement('style', {key: 3}, `@keyframes blink-motion { 0% { opacity: .1; } 50% { opacity: 1; } 100% { opacity: 0.1; } }`)
-		];
-	}
-  }
-}
+      // Don't forget to propagate it to HOC chain
+      if (this.props.onDecorated) this.props.onDecorated(terms);
+    }
+
+    render() {
+      return [
+        React.createElement(
+          Terms,
+          Object.assign({}, this.props, {
+            onDecorated: this.onDecorated,
+            key: 1
+          })
+        ),
+        this.state.isRecording &&
+          React.createElement('div', {
+            key: 2,
+            className: 'IsRecording',
+            style: {
+              animation: 'blink-motion 1s infinite',
+              position: 'absolute',
+              borderRadius: '50%',
+							top:
+							document
+                  .querySelector('.header_appTitle') ?
+                document
+                  .querySelector('.header_appTitle')
+                  .getBoundingClientRect().top + 2 : 'initial',
+              left: document.querySelector('.header_appTitle') ? document.querySelector('.header_appTitle').offsetLeft - 16 : 'initial',
+              width: 9,
+              height: 9,
+              border: '1px solid black',
+              boxShadow: '0 0 5px red',
+              backgroundColor: 'red'
+            }
+          }),
+        React.createElement(
+          'style',
+          { key: 3 },
+          `@keyframes blink-motion { 0% { opacity: .1; } 50% { opacity: 1; } 100% { opacity: 0.1; } }`
+        )
+      ];
+    }
+  };
+};
 
 // Adding Keymaps
 exports.decorateKeymaps = keymaps => {
   const newKeymaps = {
-	"window:togglerecord": "ctrl+shift+r"
+    'window:togglerecord': 'ctrl+shift+r'
   };
   return Object.assign({}, keymaps, newKeymaps);
 };
---
+++ recording.js
@@ -0,0 +1,112 @@
+const { desktopCapturer, app } = require('electron');
+const path = require('path');
+const fs = require('fs');
+const saveAs = require('file-saver').saveAs;
+const prompt = require('electron-prompt');
+let mediaRecorder;
+let title;
+
+function handleStream(stream) {
+  document.title = `[RECORDING] ${title}`;
+  mediaRecorder = new MediaRecorder(stream);
+  const chunks = [];
+
+  mediaRecorder.start();
+  console.log(mediaRecorder.state);
+  console.log('recorder started');
+  mediaRecorder.ondataavailable = function(e) {
+    chunks.push(e.data);
+  };
+
+  /* Return promise to stream buffer if successful */
+  return new Promise((resolve, reject) => {
+    mediaRecorder.onstop = () => {
+      document.title = title;
+      console.log('recorder stopped');
+      resolve(
+        new Blob(chunks, {
+          type: 'video/webm'
+        })
+      );
+    };
+
+    /* Reject recording if longer than 10min */
+    setTimeout(() => reject(null), 10 * 60 * 1000);
+  });
+}
+
+function saveStream(stream) {
+  // Logic to save blob to fs
+
+  handleStream(stream)
+    .then(blob => {
+      saveAs(blob, 'test.webm');
+      //   const reader = new FileReader();
+      //   reader.onload = function() {
+      //     const buffer = new Buffer.from(new Uint8Array(reader.result));
+      //     const appPath = path.resolve('./tmp');
+      //     const downloadsFolderName = 'Hyper-Orama';
+      //     const downloadsPathName = path.join(appPath, downloadsFolderName);
+      //     if (fs.existsSync(downloadsPathName)) {
+      //       fs.mkdir(downloadsPathName);
+      //     }
+      //     const pathName = path.join(downloadsPathName, 'my-clip.webm');
+      //     fs.writeFile(pathName, buffer, {}, (err, res) => {
+      //       if (err) {
+      //         console.error(err);
+      //         return;
+      //       }
+      //       console.log('video saved');
+      //     });
+      //   };
+
+      //   reader.readAsArrayBuffer(blob);
+    })
+    .catch(e => {
+      console.error(e);
+    });
+}
+
+function handleUserMediaError(e) {
+  console.log('getUserMediaError: ' + JSON.stringify(e, null, '---'));
+}
+
+exports.startRecording = function() {
+  title = document.title;
+  document.title = `[STARTING VIDEO CAPTURE] ${title}`;
+  desktopCapturer.getSources({ types: ['window', 'screen'] }, function(
+    error,
+    sources
+  ) {
+    for (let source of sources) {
+      if (source.name === document.title) {
+        console.log('starting');
+        console.log(source);
+        navigator.webkitGetUserMedia(
+          {
+            audio: false,
+            video: {
+              mandatory: {
+                chromeMediaSource: 'desktop',
+                chromeMediaSourceId: source.id,
+                minWidth: 1280,
+                maxWidth: 1280,
+                minHeight: 720,
+                maxHeight: 720
+              }
+            }
+          },
+          saveStream,
+          handleUserMediaError
+        );
+        return;
+      }
+    }
+  });
+};
+
+exports.stopRecording = function() {
+  mediaRecorder.stop();
+  console.log(mediaRecorder.state);
+  console.log('recorder stopped');
+};
