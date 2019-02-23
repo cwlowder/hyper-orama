7c072618357efb607237739b70580ca6653bc728
cwlowder <no>
2019-02-23 01:38:38 -0600

    Init commit

---
+++ index.js
@@ -0,0 +1,44 @@
+exports.decorateTerms = (Terms, {React}) => {
+  return class extends React.Component {
+	constructor(props, context) {
+	  super(props, context);
+	  this.terms = null;
+	  this.onDecorated = this.onDecorated.bind(this);
+	}
+
+	onDecorated(terms) {
+		this.terms = terms;
+
+		this.terms.registerCommands({
+			"window:togglerecord": e => {
+				// e parameter is React key event
+				e.preventDefault();
+				alert("I LIKE CHEESE")
+			}
+		}
+	);
+
+	  // Don't forget to propagate it to HOC chain
+	  if (this.props.onDecorated) this.props.onDecorated(terms);
+	}
+
+	render() {
+	  return React.createElement(
+		Terms,
+		Object.assign({}, this.props, {
+		  onDecorated: this.onDecorated
+		})
+	  );
+	  // Or if you use JSX:
+	  // <Terms onDecorated={this.onDecorated} />
+	}
+  }
+}
+
+// Adding Keymaps
+exports.decorateKeymaps = keymaps => {
+  const newKeymaps = {
+	"window:togglerecord": "ctrl+alt+r"
+  };
+  return Object.assign({}, keymaps, newKeymaps);
+};
